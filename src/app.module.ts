ee190bd4055cddde08611254e25d71d460813bf4
Carolina Rubio <carorubio1997@gmail.com>
2025-06-08 20:11:20 -0300

    controladores, servicios y moduloes de las entidades

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PersonsModule } from './entities/persons/persons.module';
+import { CityController } from './city/city.controller';
+import { CityModule } from './city/city.module';
+import { ProvinceController } from './province/province.controller';
+import { ProvinceModule } from './province/province.module';
+import { CountryController } from './country/country.controller';
+import { CountryModule } from './country/country.module';
 
 @Module({
-  imports: [PersonsModule],
-  controllers: [AppController],
+  imports: [PersonsModule, CityModule, ProvinceModule, CountryModule],
+  controllers: [AppController, CityController, ProvinceController, CountryController],
   providers: [AppService],
 })
 export class AppModule { }
