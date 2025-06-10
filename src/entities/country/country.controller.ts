2f95da6303e40db601e74be63c20b1d76a5b4055
Felipe <felipegiovanardi19@gmail.com>
2025-06-10 19:59:41 -0300

    Realice los endpoints de la entidad Country

--- src/entities/country/country.controller.ts
+++ src/entities/country/country.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Post, Put, Delete } from '@nestjs/common';
+import { CreateCountry } from './createCountry';
 
 @Controller('country')
-export class CountryController {}
+export class CountryController {
+    @Get()
+    get() {
+        return {
+            id: Number, name: String,
+        }
+    }
+    @Get('/country/:id')
+    getid() {
+        return {
+            id: Number, name: String,
+        }
+    }
+    @Post()
+    post(@Body() CreateCountry: CreateCountry) {
+        return {
+            id: CreateCountry.id, name: CreateCountry.name,
+        }
+    }
+    @Put('/country/:id')
+    put(@Body() CreateCountry: CreateCountry) {
+        return {
+            id: CreateCountry.id, name: CreateCountry.name,
+        }
+    }
+    @Patch('/country/:id')
+    patch(@Body() CreateCountry: CreateCountry) {
+        return {
+            id: CreateCountry.id, name: CreateCountry.name,
+        }
+    }
+    @Delete('/country/:id')
+    delete() {
+        return { message: "deleted" }
+    }
+
+
+}
+
+
+
