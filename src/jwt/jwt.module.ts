b146aff150e636d3f01dd769f8da93ea309b9fad
Felipe <felipegiovanardi19@gmail.com>
2025-06-16 23:16:44 -0300

    agregue los cambios en el JWT

--- src/jwt/jwt.module.ts
+++ src/jwt/jwt.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { JwtService } from './jwt.service';
+import { JwtAuthGuard } from './middleware/guards/auth.guard';
 
 @Module({
-  providers: [JwtService],
-  exports: [JwtService],
+  providers: [JwtService, JwtAuthGuard],
+  exports: [JwtService, JwtAuthGuard],
+
 })
-export class JwtModule {}
+export class JwtModule { }
