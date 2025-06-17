6a24baf357e9c7968950925b285ab0677452779b
Felipe <felipegiovanardi19@gmail.com>
2025-06-16 22:33:11 -0300

    aguregue las carpetas de middleware y guard para persona y JWT

--- src/country/country.module.ts
+++ src/country/country.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CountryService } from './country.service';
+import { CountryController } from './country.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  providers: [CountryService]
+  providers: [CountryService],
+  controllers: [CountryController],
+  exports: [CountryService],
+  imports: [TypeOrmModule.forFeature([CountryService])],
 })
-export class CountryModule {}
+export class CountryModule { }
